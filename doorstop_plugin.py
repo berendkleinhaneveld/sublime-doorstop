2e585ac2331b44fc605de444ba55a119c7e010f1
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2021-04-07 20:57:11 +0200

    Highlight and lint references

--- doorstop_plugin.py
+++ doorstop_plugin.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
 import subprocess
+import yaml
 
 import sublime
 import sublime_plugin
@@ -14,6 +15,10 @@ settings = None
 
 
 def trace():
+    # NOTE to future me:
+    # Start Sublime with:
+    # ▶ /Applications/Sublime\ Text.app/Contents/MacOS/Sublime\ Text
+    # then call this function to start debugger
     import pdb
     import sys
 
@@ -36,14 +41,176 @@ def plugin_unloaded():
             del globals()[key]
 
 
+class Reference:
+    def __init__(self, region, path=None, file=None, keyword=None, point=None):
+        self.region = region
+        self.path = path
+        self.file = file
+        self.keyword = keyword
+        self.point = point
+
+    def is_valid(self):
+        if not self.path:
+            return False
+        if self.keyword and not self.point:
+            return False
+        return True
+
+
+class DoorstopReferencesListener(sublime_plugin.ViewEventListener):
+    @classmethod
+    def is_applicable(cls, settings):
+        return "yaml" in settings.get("syntax").lower()
+
+    def update_regions(self):
+        # Find references key
+        references_regions = self.view.find_all(r"^references:$")
+        if len(references_regions) != 1:
+            return
+
+        references_region = references_regions[0]
+
+        lines_that_start_with_word = self.view.find_all(r"^\w+")
+
+        attribute_after_references = None
+        for x in lines_that_start_with_word:
+            if x.begin() > references_region.begin():
+                attribute_after_references = x
+
+        regions = []
+        items = self.view.find_all(r"(?s)*(^- .*?)(?:(?!^[-|\w]).)*")
+        for item in items:
+            if item.begin() < references_region.begin():
+                continue
+            if item.begin() > attribute_after_references.begin():
+                continue
+            regions.append(sublime.Region(item.begin(), item.end()))
+
+        self.references = [self.region_to_reference(region) for region in regions]
+
+        self.view.add_regions(
+            "doorstop:valid",
+            [ref.region for ref in self.references if ref.is_valid()],
+            "string",  # scope
+            "bookmark",
+            sublime.DRAW_NO_FILL,
+        )
+        self.view.add_regions(
+            "doorstop:invalid",
+            [ref.region for ref in self.references if not ref.is_valid()],
+            "invalid",  # scope
+            "bookmark",
+            # sublime.DRAW_STIPPLED_UNDERLINE
+            # | sublime.DRAW_NO_OUTLINE
+            # |
+            sublime.DRAW_NO_FILL,
+        )
+
+    def on_activated_async(self):
+        """
+        Called when a view gains input focus.
+        """
+        self.update_regions()
+
+    def parse_reference_region(self, region):
+        text = self.view.substr(region)
+        try:
+            content = yaml.load(text, Loader=yaml.SafeLoader)
+            return content[0]
+        except Exception:
+            print("Could not parse region: {}".format(text))
+        return None
+
+    def region_to_reference(self, region):
+        parsed = self.parse_reference_region(region)
+        path = parsed.get("path")
+        keyword = parsed.get("keyword")
+
+        reference = Reference(region, path=path, keyword=keyword)
+
+        if not path:
+            return reference
+
+        root = Path(self.view.window().folders()[0])
+        file = root / path
+        if not file.is_file():
+            return reference
+
+        reference.file = str(file)
+
+        with open(str(file), mode="r", encoding="utf-8") as fh:
+            point = 0
+            line = fh.readline()
+            while line:
+                if keyword in line:
+                    reference.point = point
+                    break
+                point += len(line)
+                line = fh.readline()
+
+        return reference
+
+    def on_hover(self, point, hover_zone):
+        if not hasattr(self, "references"):
+            return
+
+        hovered_references = [
+            ref for ref in self.references if ref.region.contains(point)
+        ]
+        if len(hovered_references) > 0:
+            hovered_reference = hovered_references[0]
+            href = hovered_reference.path
+            if hovered_reference.point:
+                href += ":" + str(hovered_reference.point)
+
+            if href:
+                self.view.show_popup(
+                    "<a href='{}'>{}</a>".format(href, hovered_reference.path),
+                    sublime.HIDE_ON_MOUSE_MOVE_AWAY,
+                    point,
+                    500,
+                    500,
+                    self.link_clicked,
+                )
+
+    def link_clicked(self, href):
+        root = Path(self.view.window().folders()[0])
+        try:
+            idx = href.index(":")
+        except ValueError:
+            self.view.window().open_file(str(root / href))
+            return
+
+        file = href[:idx]
+        point = href[idx + 1 :]
+
+        file_view = self.view.window().open_file(str(root / file))
+        file_view.show_at_center(int(point))
+        file_view.sel().clear()
+        file_view.sel().add(sublime.Region(int(point), int(point)))
+
+    def on_deactivated(self):
+        """
+        Called when a view loses input focus.
+        """
+        self.view.erase_regions("doorstop:invalid")
+        self.view.erase_regions("doorstop:valid")
+
+    def on_modified_async(self):
+        """
+        Called after changes have been made to a view. Runs in a separate thread, and
+        does not block the application.
+        """
+        self.update_regions()
+
+
 class DoorstopSetDoorstopPythonInterpreterCommand(sublime_plugin.ApplicationCommand):
     def run(self, interpreter):
         global settings
         settings.set(Setting().INTERPRETER, interpreter)
-        # FIXME: this might not work... Only seems to update the settings for
-        # the current view :'( There does not seem to be a way to update the setting
-        # for the user...
         settings.save()
+
+        # TODO: maybe use set_project_data(data) of WindowCommand instead?
 
     def input(self, args):
         return DoorstopPythonInterpreterInputHandler()
@@ -65,7 +232,8 @@ class DoorstopPythonInterpreterInputHandler(sublime_plugin.TextInputHandler):
 class DoorstopDebugCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         global settings
-        print(settings.get(Setting().INTERPRETER))
+        for setting in Setting():
+            print("{}: {}".format(setting, settings.get(setting)))
 
 
 def _reference(view: sublime.View):
@@ -205,7 +373,6 @@ class DoorstopFindDocumentInputHandler(sublime_plugin.ListInputHandler):
         return [item["prefix"] for item in items]
 
     def next_input(self, args):
-        # print("--> args: {}".format(args))
         if self.next_input_type:
             return self.next_input_type(self.root, args["document"])
         return None
